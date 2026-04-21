b0ccce883ece811d8f87c0c4e07a78efc89462e2
veera-vijay <veera53163@gmail.com>
2026-04-21 10:20:08 +0530

    new commit

--- vite.config.js
+++ vite.config.js
@@ -7,13 +7,4 @@ export default defineConfig({
   server: {
     port: 3000
   }
-})import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: './',  // இதை './' ஆக மாத்துங்க
-  server: {
-    port: 3000
-  }
 })
